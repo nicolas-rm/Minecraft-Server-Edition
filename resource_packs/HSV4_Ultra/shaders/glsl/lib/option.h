b9f4d0736288af9fdaab28367b18df6de7883341
Nicolas RM <nicolas.rincon@bmtask.com>
2022-04-09 01:27:33 -0500

    DESPUES

---
+++ resource_packs/HSV4_Ultra/shaders/glsl/lib/option.h
@@ -0,0 +1,80 @@
+/*=============❲PLEASE READ❳==============
+★ NOTE:
+> Cloud reflection, sun reflection, water wave, and galaxy will make your gameplay "lag" if the specification of your device is low. <
+
+> Glowing ores only support with vanilla texture. sorry this feature isn't support any texturepack:‹ <
+========================================*/
+
+/*===============❲OPTION❳=================
+★ How to modify?
+delete "//" before "#define" to activate
+write "//" before "#define" to deactivate
+
+★ Example: 
+#define name is active
+//#define name is not active
+
+★ Other Example:
+#define WATER_WAVE the water wave will be active
+//#define WATER_WAVE the water wave won't be active
+========================================*/
+
+/*================❲BASIC FEATURES❳==============*/
+
+// Bloom sun
+#define BLOOM_SUN
+
+// if you deactivate it, the shape of the sun will change to square (vanilla sun)
+#define SUN_SHAPE
+
+// twinkling light torch
+//#define TWINGKLING_LIGHT
+
+// Glowing ores 
+#define GLOWING_ORES
+
+/*==========❲CHEAT FEATURES❳=========*/
+
+// Night vision
+//#define NIGHT_VISION
+
+// You can see entities although they're blocked by a blocks (chest and players too)
+//#define X_RAY_ENTITIES
+
+/*============❲LAGGY FEATURES❳=============*/
+
+// Cloud
+#define CLOUD
+
+//Double layer cloud
+#define DOUBLE_LAYER_CLOUDS
+
+// Shadow
+#define SHADOW
+
+// Atmosphere fog
+#define ATMOSPHERE
+
+// Water wave
+#define WATER_WAVE
+
+// Grass & leaves wave
+#define LEAVES_WAVE
+
+// Cloud reflection
+#define CLOUD_REF
+
+// Sun reflection
+#define SUN_REF
+
+// Rain wet effect
+#define WET_EFFECT
+
+// Galaxy end sky
+#define GALAXY
+
+// Vanilla with glowing ores. this shader will be looks like vanilla shader but a little brighter (Enhanced Vanilla) but there's no default cloud. If you activate this feature, you won't be able to activate other features except glowing ores and cheat features.
+//#define VANILLA
+
+/*=====================================*/
+
